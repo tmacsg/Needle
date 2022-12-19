fbc824b4e1f2aa1e83a62220695a2de29eb9f9d4
bingbing <wantmac.bingbing91@gmail.com>
2022-12-20 03:43:53 +0800

    added conv_transposed prototype and test script. run test in test.ipynb

--- python/needle/nn.py
+++ python/needle/nn.py
@@ -328,7 +328,33 @@ class Conv(Module):
 
         ### END YOUR SOLUTION
 
+class Conv_transposed(Module):
+    """
+    Multi-channel 2D transposed convolutional layer
+    IMPORTANT: Accepts inputs in NCHW format, outputs also in NCHW format
+    Only supports padding=same
+    No grouped convolution or dilation
+    Only supports square kernels
+    """
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, bias=True, device=None, dtype="float32"):
+        super().__init__()
+        if isinstance(kernel_size, tuple):
+            kernel_size = kernel_size[0]
+        if isinstance(stride, tuple):
+            stride = stride[0]
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
 
+        ### BEGIN YOUR SOLUTION
+        pass
+        ### END YOUR SOLUTION
+
+    def forward(self, x: Tensor) -> Tensor:
+        ### BEGIN YOUR SOLUTION 
+        pass
+        ### END YOUR SOLUTION
 
 class RNNCell(Module):
     def __init__(self, input_size, hidden_size, bias=True, nonlinearity='tanh', device=None, dtype="float32"):
